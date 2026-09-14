9bdf2626bd82a424faecca72e8e8a8b41b0db5a5
Cynthia Aghaogwu <cynthiaaghaogwu011@gmail.com>
2026-09-14 11:19:17 +0100

    Remove duplicate frontend files

--- checkout.js
+++ checkout.js
@@ -1,28 +1,17 @@
 //Checkout
-
 const placeOrderBtn = document.getElementById("placeOrderBtn");
-
-
 if (placeOrderBtn) {
-
     placeOrderBtn.addEventListener("click", (e) => {
-
         e.preventDefault();
-
-
         const cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-
         if (cart.length === 0) {
             alert("Your cart is empty.");
             return;
         }
-
         // Check required fields
         const fullName = document.getElementById("fullName").value.trim();
         const email = document.getElementById("email").value.trim();
         const address = document.getElementById("address").value.trim();
-
         if (fullName === "" || email === "" || address === "") {
             showCartMessage("Please fill in all required fields.");
             return;
@@ -33,20 +22,13 @@ if (placeOrderBtn) {
             showCartMessage("Please enter a valid email address.");
             return;
         }
-
         // Generate order number
         const orderNumber = "#" + Date.now();
-
         localStorage.setItem("orderNumber", orderNumber);
-
 
         // Clear cart after order
         localStorage.removeItem("cart");
-
-
         // Go to success page
         window.location.href = "success.html";
-
     });
-
 }
